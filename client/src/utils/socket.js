b7ff97600be3b253098162a966c9a643c088352e
amaan-mohib <amaan.mohib@hotmail.com>
2022-09-18 15:11:14 +0530

    prod fix

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 export const ENDPOINT = import.meta.env.DEV
   ? "http://localhost:5000"
-  : "https://live-polling-system.herokuapp.com/";
+  : "https://live-polling-system.herokuapp.com";
 
 const socket = io(ENDPOINT, {
   transports: ["websocket", "polling", "flashsocket"],
